88ba2d47f75b2d13c6e481a3b03799008a381a3c
Ahmet Zurnaci <cengamat@gmail.com>
2021-04-10 10:43:31 +0100

    Navigating Back & To Next Page

--- burger-builder/src/containers/Checkout/Checkout.js
+++ burger-builder/src/containers/Checkout/Checkout.js
@@ -10,10 +10,26 @@ class Checkout extends Component {
       meat: 2,
     },
   };
+
+  checkoutCancelledHandler = () => {
+      this.props.history.goBack();
+  }
+
+  checkoutContinuedHandler = () => {
+      this.props.history.replace('/checkout/contact-data');
+  }
+  
+  
+
+
   render() {
     return (
       <div>
-        <CheckoutSummary ingredients={this.state.ingredients} />
+        <CheckoutSummary
+          ingredients={this.state.ingredients}
+          checkoutCancelled={this.checkoutCancelledHandler}
+          checkoutContinued={this.checkoutContinuedHandler}
+        />
       </div>
     );
   }
