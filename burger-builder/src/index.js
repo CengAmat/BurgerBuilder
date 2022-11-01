8ef8565bad7fadf29b88c8eea567cc19fa8b3821
cengamat <azurnaci@cultwinesltd.com>
2022-11-01 14:57:58 +0000

    Connecting the Burger Builder Container To Redux

--- burger-builder/src/index.js
+++ burger-builder/src/index.js
@@ -2,14 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { configureStore } from "@reduxjs/toolkit";
+import { createStore } from "redux";
 
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import reducer from "./store/reducer";
 
-const store = configureStore(reducer);
+const store = createStore(reducer);
 
 const app = (
   <Provider store={store}>
