0a2bb59cd584dbca0e64edb2d74583b98e6ee5ef
cengamat <azurnaci@cultwinesltd.com>
2022-11-01 13:54:12 +0000

    Installed Redux, Setting Up and Connectiong Store

--- burger-builder/src/index.js
+++ burger-builder/src/index.js
@@ -1,20 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import reducer from "./store/reducer";
+
+const store = configureStore(reducer);
 
 const app = (
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
 
 ReactDOM.render(
-  <React.StrictMode>
-    {app}
-  </React.StrictMode>,
+  <React.StrictMode>{app}</React.StrictMode>,
   document.getElementById("root")
 );
 
